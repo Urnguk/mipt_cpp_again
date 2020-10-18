c5c30b3868094ba1597fcc60bf746853b3fe989e
Urnguk <spiri-o-wate@mail.ru>
2020-10-19 02:14:39 +0300

    Create source.cpp

---
+++ 08.10.2020/class_particle/source.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+#include <algorithm>
+#include <cmath>
+#include <functional>
+
+#include "particle.hpp"
+
+int main()
+{
+	const double time = 10.28;
+
+	Particle proton(0.0, 0.0, 0.0, 1, 1);
+	Particle alpha(0.5, 0.5, 0.0, 4, 2);
+
+	interaction(proton, alpha);
+	proton.move(time);
+	alpha.move(time);
+
+	print(proton);
+	print(alpha);
+
+	return 0;
+}
