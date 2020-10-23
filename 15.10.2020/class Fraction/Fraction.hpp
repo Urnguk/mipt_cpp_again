ab7732100c01e035f95ba3487da7729cc5b9ab4b
Urnguk <spiri-o-wate@mail.ru>
2020-10-24 00:44:03 +0300

    Update Fraction.hpp

--- 15.10.2020/class Fraction/Fraction.hpp
+++ 15.10.2020/class Fraction/Fraction.hpp
@@ -9,6 +9,8 @@ class Fraction
 {
 public:
 	Fraction(int x = 0, int y = 1);
+	Fraction(const Fraction& fraction);
+	Fraction(Fraction&& fraction);
 	~Fraction() = default;
 
 	inline int get_up() const
@@ -19,24 +21,36 @@ public:
 	{
 		return down;
 	}
-	inline void set_up(int x)
-	{
-		up = x;
-		reduction();
-	}
-	inline void set_down(unsigned int y)
-	{
-		down = y;
-		reduction();
-	}
+	void set_up(int x);
+	void set_down(int y);
 
 	Fraction& operator++();
 	Fraction& operator++(int);
-
 	Fraction& operator--();
 	Fraction& operator--(int);
 
+	Fraction& operator+=(const Fraction& other);
+	Fraction& operator-=(const Fraction& other);
+	Fraction& operator*=(const Fraction& other);
+	Fraction& operator/=(const Fraction& other);
+
+	friend Fraction operator+(const Fraction& frac_1, const Fraction& frac_2);
+	friend Fraction operator-(const Fraction& frac_1, const Fraction& frac_2);
+	friend Fraction operator*(const Fraction& frac_1, const Fraction& frac_2);
+	friend Fraction operator/(const Fraction& frac_1, const Fraction& frac_2);
+
+	friend bool operator==(const Fraction& frac_1, const Fraction& frac_2);
+	friend bool operator!=(const Fraction& frac_1, const Fraction& frac_2);
+	friend bool operator<=(const Fraction& frac_1, const Fraction& frac_2);
+	friend bool operator>=(const Fraction& frac_1, const Fraction& frac_2);
+	friend bool operator<(const Fraction& frac_1, const Fraction& frac_2);
+	friend bool operator>(const Fraction& frac_1, const Fraction& frac_2);
+
+
 	friend std::ostream& operator<< (std::ostream& out, const Fraction& fraction);
+	friend std::istream& operator>> (std::istream& in, Fraction& fraction);
+
+	explicit operator double() const { return double(up) / double(down); }
 
 private:
 	int up;
