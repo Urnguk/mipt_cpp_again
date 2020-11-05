68d6fdf52972f1d2106430659f7ad54e931befdc
Urnguk <spiri-o-wate@mail.ru>
2020-11-06 00:50:07 +0300

    Update source.cpp

--- 5.11.2020/class Container/source.cpp
+++ 5.11.2020/class Container/source.cpp
@@ -2,5 +2,12 @@
 
 int main()
 {
-
+	Container <int> A(0);
+	A.push(5, 0);
+	A.set(3, 1);
+	Container <int> B(1);
+	B = A;
+	std::cout << B.get(0) << " ";
+	std::cout << B.pop(1) << " ";
+	std::cout << B.pop(0);
 }
