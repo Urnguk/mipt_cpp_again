39d7a1acbcddefdd95e23f518f0336d2ae9c5a6a
Urnguk <spiri-o-wate@mail.ru>
2020-11-08 15:55:33 +0300

    Update source.cpp

--- 5.11.2020/class Container/source.cpp
+++ 5.11.2020/class Container/source.cpp
@@ -2,12 +2,19 @@
 
 int main()
 {
+	int N = 0;
+	std::cin >> N;
+
+	int* array = new int[N];
+	for (auto i = 0U; i < N; ++i)
+	{
+		std::cin >> array[i];
+	}
 	Container <int> A(0);
-	A.push(5, 0);
-	A.set(3, 1);
-	Container <int> B(1);
-	B = A;
-	std::cout << B.get(0) << " ";
-	std::cout << B.pop(1) << " ";
-	std::cout << B.pop(0);
+	Container <int> B(array, N);
+	swap(A, B);
+	std::cout << A[0] << " " << A[1] << " "<< B[0] << std::endl;
+	B[0] = 4;
+	std::cout << B[0];
+	return 0;
 }
