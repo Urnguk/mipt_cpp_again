e3f003b1009213086e4c636773a8e231711a366b
Urnguk <spiri-o-wate@mail.ru>
2020-11-12 16:13:09 +0300

    CRTP applied

--- 12.11.2020/CRTP.cpp
+++ 12.11.2020/CRTP.cpp
@@ -3,9 +3,6 @@
 class My_class
 {
 public:
-	My_class() = default;
-	~My_class() = default;
-
 	virtual void print()
 	{
 		std::cout << "My class was printed" << std::endl;
@@ -15,19 +12,42 @@ public:
 class My_special_class : public My_class
 {
 public:
-	My_special_class() = default;
-	~My_special_class() = default;
-
 	virtual void print() override
 	{
 		std::cout << "My special class was printed" << std::endl;
 	}
 };
 
+template <typename Type>
+class Base
+{
+private:
+	Type* Me()
+	{
+		return static_cast<Type*>(this);
+	}
+public:
+	void print()
+	{
+		Me()->print();
+	}
+};
+
+class Heir : public Base<Heir>
+{
+public:
+	friend class Base<Heir>;
+
+	void print()
+	{
+		std::cout << "Heir was printed" << std::endl;
+	}
+};
+
 int main()
 {
-	My_class* A = new My_class();
-	My_class* B = new My_special_class();
+	My_class* A = new My_special_class();
+	Base<Heir>* B = new Base<Heir>();
 
 	A->print();
 	B->print();
